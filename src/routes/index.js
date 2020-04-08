468dc60b27cc8ca607deb2751968edbea85d4aa5
Karla Agraz <karla.agraz@gmail.com>
2020-04-07 23:02:33 -0500

    Obtener personajes con la función de llamado a la API

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,12 +9,18 @@ const routes = {
     '/': Home,
     '/:id': Character,
     '/contact': 'Contact',
-}
+};
+
 const router = async () => {
     const header = null || document.getElementById('header');
     const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
+    let hash = getHash();
+    let route = await resolveRoutes(hash);
+
+    let render = routes[route] ? routes[route] : Error404;
+    content.innerHTML = await render();
 };
 
 export default router;
